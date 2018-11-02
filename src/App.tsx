36ed5181fada0a1061ab3819366fac7607aff98e
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-11-02 12:38:34 +0100

    :sparkles: adding suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React, { Suspense, lazy } from 'react';
-import { Spin } from 'antd';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Menu from './components/Header';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import { Spin } from "antd";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Menu from "./components/Header";
+import "./App.css";
 
-const Home = lazy(() => import('./components/Home'));
+const Home = lazy(() => import("./components/Home"));
+const User = lazy(() => import("./components/User"));
 function App() {
   return (
     <>
-      <Menu />
       <Router>
-        <Suspense fallback={<Spin />}>
-          <Route exact path="/home" component={Home} />
-        </Suspense>  
-      </Router>  
+        <>
+          <Menu />
+          <div className="App">
+            <Suspense fallback={<Spin />}>
+              <Route exact path="/home" component={Home} />
+              <Route path="/user" component={User} />
+            </Suspense>
+          </div>
+        </>
+      </Router>
     </>
   );
 }
-
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,36 @@
-import { Menu, Icon } from 'antd';
-import React, { useState } from 'react';
+import { Menu, Icon } from "antd";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Item = Menu.Item;
 
 export default function Header() {
-  const [current, setCurrent] = useState<string>('home');
+  const [current, setCurrent] = useState<string>("home");
   function onChange(e: { key: string }) {
     console.log({ e });
     setCurrent(e.key);
   }
   return (
-    <Menu
-      selectedKeys={[current]}
-      theme={"light"}
-      mode={"horizontal"}
-      onClick={onChange}
-    >
-      <Item key="home">
-        <Icon type="home" />
-        Home
-      </Item>
-      <Item key="user">
-        <Icon type="user" />
-        User
-      </Item>
-    </Menu>
-  )
+    <Router>
+      <Menu
+        selectedKeys={[current]}
+        theme={"light"}
+        mode={"horizontal"}
+        onClick={onChange}
+      >
+        <Item key="home">
+          <Link to="/home">
+            <Icon type="home" />
+            Home
+          </Link>
+        </Item>
+        <Item key="user">
+          <Link to="/user">
+            <Icon type="user" />
+            User
+          </Link>
+        </Item>
+      </Menu>
+    </Router>
+  );
 }

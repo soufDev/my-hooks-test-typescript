e56f219193ee4057b552d4e0912ada8875c71f13
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-11-11 23:22:56 +0100

    :sparkles: add new stuffs in the home pageé

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+const tab = [1, -2, 7, -6];
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <h3>[{ tab.map((e, i) => i===0 ? e: ','+e) }] sorted gives [{tab.sort((a,b) => a-b).map((e, i) => i===0 ? e: ','+e)}]</h3>
+    </>
+
   )
 }
